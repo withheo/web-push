c172f0f69a16bcf0bbe547cc66910e218c327fda
withHeo <mynet81@gmail.com>
2024-02-01 18:43:52 +0900

    FIX: store

--- src/cropto.js
+++ src/cropto.js
@@ -2,13 +2,13 @@ const crypto = require('crypto-js');
 
 const cryptoKey = 'noti-server';
 
-
-
+///const firebaseServiceAccount2 = require('./config/firebaseServiceAccountKeys.json');
 
 const decrypt = (text, key) => {
   try {
     //console.log(text);
     const str = crypto.AES.decrypt(text, key);
+    console.log(str.toString(crypto.enc.Utf8));
     return JSON.parse(str.toString(crypto.enc.Utf8));
   } catch(err) {
     console.log(err);
@@ -18,13 +18,21 @@ const decrypt = (text, key) => {
 
 const encrypt = () => {
   
-  //const a = crypto.AES.encrypt(JSON.stringify(firebaseConfig_message), "noti-server").toString();
+ // const a = crypto.AES.encrypt(JSON.stringify(firebaseConfig), cryptoKey).toString();
   //const b = crypto.AES.encrypt(JSON.stringify(firebaseConfig2), "noti-server").toString();
 
   //console.log(a);
   //console.log(" **** ")
  
   //console.log(b);
+   // return a;
 }
 
+// const x = encrypt();
+// const a = decrypt(x, cryptoKey);
+// console.log(a);
+
+// const firebaseAdminApp = firebaseAdmin.initializeApp({
+//   credential: firebaseAdmin.credential.cert(firebaseServiceAccount)
+// });
 // encrypt();
